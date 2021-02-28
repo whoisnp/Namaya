402e35da1567009e30aa947a24c8c7c118b1fa41
Nithya-Prakash <nithyap322@gmail.com>
2021-02-28 12:56:04 +0530

    initial commit

---
+++ assets/js/crud.js
@@ -0,0 +1,22 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// var firebaseConfig = {
+//   apiKey: "AIzaSyCmJCdJ5y8Px2gn7O6PTq_MxVx30Q4zZDM",
+//   authDomain: "projecttkr-4aa57.firebaseapp.com",
+//   projectId: "projecttkr-4aa57",
+//   storageBucket: "projecttkr-4aa57.appspot.com",
+//   messagingSenderId: "353303900963",
+//   appId: "1:353303900963:web:6ee6c1e0a0e774c2851296",
+//   measurementId: "G-12RFY7K0EN",
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+// appfrm = document.querySelector("#appfrm");
+// console.log(appfrm);
+// appfrm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   var name = appfmr["name"].value;
+//   console.log(name);
+// });

536b5d01fae0ec9c47882ed335acea3065b5886a
Nithya-Prakash <nithyap322@gmail.com>
2021-04-16 10:00:14 +0530

    removed the creds

--- config.php
+++ config.php
@@ -1,8 +1,5 @@
 <?php 
-	$host = 'localhost';
-	$DBUser = "u960537433_root";
-	$DBPassword = 'pg/aUF6|b';
-	$db = 'u960537433_NamayaLogin';
+	
 
 	
 	$conn = mysqli_connect($host,$DBUser, $DBPassword, $db);
